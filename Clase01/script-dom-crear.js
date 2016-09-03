6590629d8098fe31065e2e988ff83b3da234d2ba
Alumno <Alumno>
2016-09-03 13:40:31 -0500

    Clase01

---
+++ Clase01/script-dom-crear.js
@@ -0,0 +1,10 @@
+var elemento = document.createElement("h1");
+elemento.innerHTML = "Título en el h1";
+
+elemento.style.fontFamily = "Verdana";
+elemento.style.fontSize = "20px";
+elemento.style.color = "rgb(255, 255, 0)";
+elemento.style.fontWeight = "normal";
+elemento.style.fontStyle = "italic";
+
+document.body.appendChild(elemento);
