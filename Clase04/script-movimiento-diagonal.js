abdfd82b70db8f4120b2c46761f33b2a30993d9b
Alumno <Alumno>
2016-09-24 11:20:45 -0500

    Clase04

---
+++ Clase04/script-movimiento-diagonal.js
@@ -0,0 +1,37 @@
+var anchoBody = document.body.offsetWidth;
+var altoBody = document.body.offsetHeight;
+var circulo = document.getElementsByTagName("div")[0];
+
+var px = circulo.offsetLeft,
+	py = circulo.offsetTop,
+    anchoCirculo = circulo.offsetWidth,
+    altoCirculo = circulo.offsetHeight,
+    dx = 10,
+    dy = 10,
+    tiempo = 20;
+
+function fntMovimiento(){
+	var limiteSuperiorX = (px + anchoCirculo + dx > anchoBody);
+	var limiteInferiorX = (px + dx < 0);
+
+	var limiteSuperiorY = (py + altoCirculo + dy > altoBody);
+	var limiteInferiorY = (py + dy < 0);
+
+	if(limiteSuperiorX || limiteInferiorX) {
+		dx *= -1;
+	}
+
+	if(limiteSuperiorY || limiteInferiorY) {
+		dy *= -1;
+	}
+
+	px += dx;
+	py += dy;
+
+	circulo.style.left = (px + "px");
+	circulo.style.top = (py + "px");
+
+	setTimeout(fntMovimiento, tiempo);
+}
+
+setTimeout(fntMovimiento, tiempo);
---
+++ Clase04/script-raf-multiple.js
@@ -0,0 +1,40 @@
+var anchoBody = document.body.offsetWidth;
+var altoBody = document.body.offsetHeight;
+var circulo = document.getElementsByTagName("div")[0];
+
+var px = circulo.offsetLeft,
+	py = circulo.offsetTop,
+    anchoCirculo = circulo.offsetWidth,
+    altoCirculo = circulo.offsetHeight,
+    dx = 20,
+    dy = 20;
+
+function fntMovimiento(){
+	var limiteSuperiorX = (px + anchoCirculo + dx > anchoBody);
+	var limiteInferiorX = (px + dx < 0);
+
+	var limiteSuperiorY = (py + altoCirculo + dy > altoBody);
+	var limiteInferiorY = (py + dy < 0);
+
+	if(limiteSuperiorX || limiteInferiorX) {
+		dx *= -1;
+	}
+
+	if(limiteSuperiorY || limiteInferiorY) {
+		dy *= -1;
+	}
+
+	px += dx;
+	py += dy;
+
+	circulo.style.left = (px + "px");
+	circulo.style.top = (py + "px");
+
+	funcionAnimacion(fntMovimiento);
+}
+
+var funcionAnimacion = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || window.oRequestAnimationFrame;
+
+funcionAnimacion(fntMovimiento);
+
+// window.requestAnimationFrame(fntMovimiento);
